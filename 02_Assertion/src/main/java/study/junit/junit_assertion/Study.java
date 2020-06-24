13eff66eb61f959341b58bb36337bb7f7fb1200e
pjhsk113 <pjhsk113@naver.com>
2020-06-24 10:07:51 +0900

    [add] 02_Assertion Study

---
+++ 02_Assertion/src/main/java/study/junit/junit_assertion/Study.java
@@ -0,0 +1,10 @@
+package study.junit.junit_assertion;
+
+public class Study {
+    private StudyStatus status = StudyStatus.DRAFT;
+
+
+    public StudyStatus getStatus() {
+        return this.status;
+    }
+}
---
+++ 02_Assertion/src/main/java/study/junit/junit_assertion/StudyStatus.java
@@ -0,0 +1,5 @@
+package study.junit.junit_assertion;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
