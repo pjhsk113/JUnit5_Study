982313ce14dce8997850a6ba364d7a651248ae52
Jangduk <jhpark1@wemb.co.kr>
2020-06-25 08:49:54 +0900

    [change] 02_Assertion Study

--- 02_Assertion/src/test/java/study/junit/junit_assertion/StudyTest.java
+++ 02_Assertion/src/test/java/study/junit/junit_assertion/StudyTest.java
@@ -2,6 +2,8 @@ package study.junit.junit_assertion;
 
 import org.junit.jupiter.api.*;
 
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudyTest {
@@ -11,7 +13,12 @@ class StudyTest {
     void create_new_study() {
         Study study = new Study();
         assertNotNull(study);
-        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들때 Null이 아니어야 한다.");
+
+        // 메시지를 String 형태로 생성하면, 테스트가 성공하든 실패하든 문자열 연산을 매번 실행한다.
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 DRAFT 상태다.");
+        // 람다식을 사용하면 위의 문제를 해결할 수 있어 문자열 연산의 비용을 최소화 할 수 있다.
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 DRAFT 상태다.");
+
     }
 
     @Test
