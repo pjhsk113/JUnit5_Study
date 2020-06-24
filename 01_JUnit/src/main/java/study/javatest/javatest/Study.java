a053fc5e0c7faa1f7c018a0ebc7feb3e80c7bad8
Jangduk <jhpark1@wemb.co.kr>
2020-06-24 09:53:26 +0900

    [change] JUnit 기본_01
    - DisplayName 학습

--- 01_JUnit/src/main/java/study/javatest/javatest/Study.java
+++ 01_JUnit/src/main/java/study/javatest/javatest/Study.java
@@ -2,9 +2,4 @@ package study.javatest.javatest;
 
 public class Study {
 
-    private StudyStatus status = StudyStatus.DRAFT;
-
-    public StudyStatus getStatus() {
-        return this.status;
-    }
 }
